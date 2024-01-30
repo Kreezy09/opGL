4f8dd71d516564c61011c7430d319ee61b6a701f
Keith <keithkareithi09@gmail.com>
2024-01-30 15:30:21 +0300

    bresenhem

---
+++ bresenhem.cpp
@@ -0,0 +1,73 @@
+#include <GL/glut.h>
+#include <iostream>
+
+int x1, yi, x2, y2;
+
+void draw_pixel(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+    glFlush();
+}
+
+void draw_line(int x1, int x2, int yi, int y2) {
+    int x, y, dx, dy,i, p, step, incx, incy;
+    dx = abs(x2 - x1);
+    dy = abs(y2 - yi);  
+    incx = 1;
+    if (x2 < x1) incx = -1;
+    incy = 1;
+    if (y2 < yi) incy = -1;
+    if (dx > dy){
+        step = dx;
+    } else {
+        step = dy;
+    }
+    x = x1;
+    y = yi;
+    draw_pixel(x, y);
+    p = 2 * dy - dx;
+
+    for ( i=0; i<step; i++){
+        if ( p < 0){
+            x += incx;
+            p += 2 * dy;
+        } else {
+            x += incx;
+            y += incy;
+            p += 2 * (dy - dx);
+        }
+        draw_pixel(x, y);
+        std::cout << "x: " << x << ", y: " << y << std::endl;
+    }
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0); // Set color to white
+
+    draw_line(x1, x2, yi, y2);
+
+    glFlush();
+}
+
+void myInit() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    gluOrtho2D(-10, 10, -10, 10);
+}
+
+int main(int argc, char **argv) {
+    printf("Enter (x1, yi, x2, y2)\n");
+    scanf("%d %d %d %d", &x1, &yi, &x2, &y2);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("Bresenham Line Drawing Algorithm");
+    myInit();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
--- midpoint.cpp
+++ midpoint.cpp
@@ -1,34 +1,89 @@
 #include <GL/glut.h>
 #include <iostream>
 
-int x1 = -4, yi = 3, x2 = 5, y2 = -2;
+int x2, y2, x3, y3;
+
+void draw_pixel(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+}
+
+void draw_line(int x2, int x3, int y2, int y3) {
+    int dx, dy, i, e;
+    int incx, incy, inc1, inc2;
+    int x, y;
+
+    dx = x3 - x2;
+    dy = y3 - y2;
+
+    incx = 1;
+    if (dx < 0) incx = -1;
+    incy = 1;
+    if (dy < 0) incy = -1;
+
+    x = x2;
+    y = y2;
+
+    draw_pixel(x, y);
+
+    if (dx > dy) {
+        e = 2 * dy - dx;
+        inc1 = 2 * (dy - dx);
+        inc2 = 2 * dy;
+
+        for (i = 0; i < dx; i++) {
+            if (e >= 0) {
+                y += incy;
+                e += inc1;
+            } else {
+                e += inc2;
+            }
+            x += incx;
+            draw_pixel(x, y);
+        }
+    } else {
+        e = 2 * dx - dy;
+        inc1 = 2 * (dx - dy);
+        inc2 = 2 * dx;
+
+        for (i = 0; i < dy; i++) {
+            if (e >= 0) {
+                y += incy;
+                e += inc1;
+            } else {
+                e += inc2;
+            }
+            x += incx;
+            draw_pixel(x, y);
+        }
+    }
+}
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POINT_SMOOTH);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glColor3f(1.0, 1.0, 1.0); // Set color to white
 
-    int dx = x2 - x1;
-    int dy = y2 - yi;
-
-    int x = x1, y = yi;
-
-    glBegin(GL_POINTS);
-    glVertex2i(x, y); // Plot the initial point
-
-    int P = 2 * dy - dx; // Initial decision parameter
-
-    while (x < x2) {
-        x++;
-        if (P < 0) {
-            P += 2 * dy;
-        } else {
-            P += 2 * dy - 2 * dx;
-            y--; // Corrected: Decrement y when P_k >= 0
-        }
-        glVertex2i(x, y);
-    }
-
+    // Draw Y-axis
+    glBegin(GL_LINES);
+    glVertex2f(0, -10);  // Starting point of Y-axis
+    glVertex2f(0, 10);   // Ending point of Y-axis
     glEnd();
+
+    // Draw X-axis
+    glBegin(GL_LINES);
+    glVertex2f(-10, 0);  // Starting point of X-axis
+    glVertex2f(10, 0);   // Ending point of X-axis
+    glEnd();
+
+    glColor3f(1.0, 0.0, 0.0); // Set color to red
+    draw_line(x2, x3, y2, y3);
+
     glFlush();
 }
 
@@ -38,11 +93,14 @@ void myInit() {
 }
 
 int main(int argc, char **argv) {
+    printf("Enter (x2, y2, x3, y3)\n");
+    scanf("%d %d %d %d", &x2, &y2, &x3, &y3);
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("Midpoint Line Drawing Algorithm");
+    glutCreateWindow("Smooth Lines for Axes and Line");
     myInit();
     glutDisplayFunc(display);
     glutMainLoop();
